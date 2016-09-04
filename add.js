6bc8e0566d41b0bc5666a313486bf457f08e19a1
wuhao5436 <772930613@qq.com>
2016-09-04 19:55:39 +0800

    试验

---
+++ add.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Administrator
+* @Date:   2016-09-04 08:45:13
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-09-04 08:54:21
+*/
+
+define(function(require,exports,module){
+	module.exports = function(a,b){
+		return a+b;
+	}
+})
---
+++ calculater.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2016-09-04 08:44:41
+* @Last Modified by:   Administrator
+* @Last Modified time: 2016-09-04 08:51:09
+*/
+
+define(function(require,exports,module){
+	exports.add = require("add");
+})
